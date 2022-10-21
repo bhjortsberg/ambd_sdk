16ff62269da046ea880152015f821a657cc72986
Bjorn Hjortsberg <bjorn.hjortsberg@percepio.com>
2022-10-21 16:18:04 +0200

    Dump process stack memory region

--- project/realtek_amebaD_va0_example/src/src_hp/crash_hexdump.c
+++ project/realtek_amebaD_va0_example/src/src_hp/crash_hexdump.c
@@ -9,8 +9,8 @@ static void printString(const char* pString);
 const CrashCatcherMemoryRegion* CrashCatcher_GetMemoryRegions(void)
 {
     static const CrashCatcherMemoryRegion regions[] = {
-        /*{0x1003C800, 0x1003D800, CRASH_CATCHER_BYTE},*/
         {0x10004000, 0x10005000, CRASH_CATCHER_BYTE},
+        {0x1003CF00, 0x1003D800, CRASH_CATCHER_BYTE},
         {0xFFFFFFFF, 0xFFFFFFFF, CRASH_CATCHER_BYTE}
     };
     return regions;
